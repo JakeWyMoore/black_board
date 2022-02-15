07140ef282f13c7f324db7d54c44091ca7c4e7d5
Jacob Moore <jakewymoore@gmail.com>
2022-02-15 14:38:06 -0700

    more styles

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ function App() {
   return (
 
     <div>
-      <h1>My ToDo List</h1>
+      <h1>ToDo List</h1>
       <div className='card'>
         <h2>Title</h2>
         <button>Delete</button>
-
       </div>
     </div>
 
