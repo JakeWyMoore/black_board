20fd819c681625d9b1fc5e1baed88974b85cd201
Jacob Moore <jakewymoore@gmail.com>
2022-02-15 17:36:35 -0700

    new app

---
+++ src/NewApp.js
@@ -0,0 +1,17 @@
+// import './App.css';
+
+function NewApp() {
+  return (
+
+    <div>
+      <h1>ToDo List</h1>
+      <div className='card'>
+        <h2>Title</h2>
+        <button>Delete</button>
+      </div>
+    </div>
+
+  );
+}
+
+export default NewApp;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+import NewApp from './NewApp';
 
 
 ReactDOM.render(
   
   <App />,
+  <NewApp />,
 
   document.getElementById('root')
 );
